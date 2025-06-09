3c406c997293edec3f412a823c23298ecf5fe84a
Subham Mani <manisubham09@gmail.com>
2025-06-09 13:21:30 +0530

    add React Query support

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
+const queryClient = new QueryClient();
+
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
+  );
 };
 
 export default Providers;
