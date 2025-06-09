5c8ba224cf1cca97fe2d48ab28d5fe00c9de13df
Subham Mani <manisubham09@gmail.com>
2025-06-09 13:21:19 +0530

    update layout background styling with radial gradient

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from "react";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-gradient-to-r from-neutral-800 via-neutral-900 to-black text-white relative overflow-hidden">
+    <div className="bg-neutral-950 bg-[radial-gradient(ellipse_90%_90%_at_50%_-30%,rgba(236,72,153,0.25),rgba(96,130,246,0.25),rgba(255,255,255,0))] text-white relative overflow-hidden">
       {children}
       <Navbar />
     </div>
