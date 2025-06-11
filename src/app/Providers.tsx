0f5ac97aecc494f83c944530bce27d0947645e09
Subham Mani <manisubham09@gmail.com>
2025-06-11 22:19:56 +0530

    add toaster

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,14 +2,22 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/sonner";
 
 const queryClient = new QueryClient();
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <SessionProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <ConvexProvider client={convex}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <Toaster />
+        </QueryClientProvider>
+      </ConvexProvider>
     </SessionProvider>
   );
 };
