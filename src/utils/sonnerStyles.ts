14865c26bbda491da614750eac9963c3f7723cf0
Subham Mani <manisubham09@gmail.com>
2025-06-11 22:19:08 +0530

    add sonner and error style

---
+++ src/utils/sonnerStyles.ts
@@ -0,0 +1,11 @@
+export const error = {
+  background: "rgba(169,41,55,0.7)",
+  color: "#f9fafb",
+  border: "1px solid #e60076",
+  borderRadius: "8px",
+  boxShadow: "0 8px 32px 0 rgba(31,41,55,0.37)",
+  backdropFilter: "blur(8px)",
+  WebkitBackdropFilter: "blur(8px)",
+  fontFamily: "cursive",
+  fontWeight: "bold",
+};
