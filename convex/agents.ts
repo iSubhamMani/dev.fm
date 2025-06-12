a7250cfd34ac22c10d6cca7fc0a6d7d8f97fde68
Subham Mani <manisubham09@gmail.com>
2025-06-12 20:56:17 +0530

    add script generation agent for podcast scriptwriting

---
+++ convex/agents.ts
@@ -0,0 +1,74 @@
+import { z } from "zod";
+import { Agent } from "@convex-dev/agent";
+import { components } from "./_generated/api";
+import { tool } from "ai";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { v } from "convex/values";
+import { action } from "../convex/_generated/server";
+
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GOOGLE_AI_API_KEY || "",
+});
+
+const scriptAgentSystemPrompt = `
+    You are a podcast scriptwriting assistant for developers.
+
+    Your task is to generate or update a podcast script based on user instructions. Always return:
+    1. The updated full podcast script
+    2. A brief, natural explanation of what changes you made — e.g., what section you expanded, reworded, or added.
+
+    Return your result using the generateScriptTool with both "script" and "explanation" fields. Do not return anything else outside the tool.
+`;
+
+const generateScriptTool = tool({
+  description: ``,
+  parameters: z.object({
+    script: z.string(),
+    explanation: z.string(),
+  }),
+  execute: async (args) => {
+    return args;
+  },
+});
+
+const scriptGenerationAgent = new Agent(components.agent, {
+  chat: google("gemini-2.0-flash"),
+  instructions: scriptAgentSystemPrompt,
+  tools: { generateScriptTool },
+});
+
+export const createScriptAgentThread = action({
+  args: {
+    prompt: v.string(),
+    userId: v.string(),
+    threadId: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    let thread;
+    let threadId = args.threadId;
+
+    if (!threadId) {
+      const result = await scriptGenerationAgent.createThread(ctx, {
+        userId: args.userId,
+      });
+      thread = result.thread;
+      threadId = result.threadId;
+    } else {
+      const result = await scriptGenerationAgent.continueThread(ctx, {
+        threadId: threadId,
+        userId: args.userId,
+      });
+      thread = result.thread;
+    }
+
+    const result = await thread.generateText({
+      prompt: args.prompt,
+    });
+
+    return {
+      threadId,
+      text: result?.text,
+      toolResults: result?.toolResults?.[0]?.result,
+    };
+  },
+});
