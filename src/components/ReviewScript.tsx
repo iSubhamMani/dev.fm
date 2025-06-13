2a3ba7842b80fce13069b685e81b4977bae06c4d
Subham Mani <manisubham09@gmail.com>
2025-06-13 13:30:23 +0530

    refactor ReviewScript to integrate avatar component and enhance user experience during script generation

--- src/components/ReviewScript.tsx
+++ src/components/ReviewScript.tsx
@@ -10,7 +10,9 @@ import { useAction } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { LuLoaderCircle } from "react-icons/lu";
 import { RiRobot2Fill } from "react-icons/ri";
-import { FaUser } from "react-icons/fa";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { WordRotate } from "./magicui/word-rotate";
+import { Id } from "../../convex/_generated/dataModel";
 
 interface ChatMessage {
   role: "user" | "assistant";
@@ -21,10 +23,12 @@ const ReviewScript = ({
   podcastDetails,
   threadId,
   uid,
+  avatar,
 }: {
   podcastDetails: Podcast;
   threadId: string;
   uid: string;
+  avatar: string;
 }) => {
   const [script, setScript] = useState(podcastDetails.script || "");
   const [userPrompt, setUserPrompt] = useState<string>("");
@@ -45,12 +49,14 @@ const ReviewScript = ({
       return;
     }
     setChatMessages((prev) => [...prev, { role: "user", content: userPrompt }]);
+
     try {
       setGeneratingChanges(true);
       const { toolResults } = await createScriptAgentThreadAction({
         prompt: userPrompt,
         userId: uid,
         threadId,
+        podcastId: podcastDetails._id as Id<"podcasts">,
       });
 
       setUserPrompt("");
@@ -70,10 +76,12 @@ const ReviewScript = ({
   };
 
   return (
-    <div className="h-screen bg-[radial-gradient(ellipse_90%_90%_at_50%_-30%,rgba(236,72,153,0.25),rgba(96,130,246,0.25),rgba(255,255,255,0))] flex bg-neutral-950/95 relative overflow-hidden text-white">
+    <div
+      className={`h-screen bg-[radial-gradient(ellipse_90%_90%_at_50%_-30%,rgba(236,72,153,0.25),rgba(96,130,246,0.25),rgba(255,255,255,0))] flex bg-neutral-950/95 relative overflow-hidden text-white`}
+    >
       <section className="w-full border-l-2 border-neutral-800 flex flex-col">
         <div className="flex-1 flex flex-col overflow-y-auto rounded-sm border border-neutral-600 shadow-lg bg-neutral-900/60 backdrop-blur-sm text-white p-4">
-          <div className="flex-1 flex flex-col gap-6 overflow-y-auto">
+          <div className="flex-1 flex flex-col gap-6 overflow-y-auto px-4 pb-6 pt-2">
             {chatMessages.map((message, index) => {
               return (
                 <div
@@ -81,18 +89,35 @@ const ReviewScript = ({
                   className={` flex items-start gap-2 w-full max-w-md ${message.role === "user" ? "text-pink-300 self-end flex-row-reverse" : "text-blue-300 self-start"}`}
                 >
                   {message.role === "assistant" ? (
-                    <RiRobot2Fill className="size-4 text-blue-300 mb-2" />
+                    <RiRobot2Fill className="size-4 text-blue-300" />
                   ) : (
-                    <FaUser className="size-4 text-pink-300 mb-2" />
+                    <Avatar>
+                      <AvatarImage src={avatar} />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
                   )}
                   <div
-                    className={`shadow-lg border border-neutral-800 flex-1 bg-neutral-900 p-4 rounded-md`}
+                    className={`shadow-md border border-neutral-800 flex-1 bg-neutral-950/45 p-4 rounded-b-xl ${message.role === "user" ? "rounded-tl-xl" : "rounded-tr-xl"}`}
                   >
-                    <p className="mt-1">{message.content}</p>
+                    <p className="mt-1 ">{message.content}</p>
                   </div>
                 </div>
               );
             })}
+            {generatingChanges && (
+              <div className="w-max flex items-center gap-2 ">
+                <RiRobot2Fill className="size-4 text-blue-300" />
+                <WordRotate
+                  className="text-sm text-white font-medium animate-pulse"
+                  words={[
+                    "Analyzing your input...",
+                    "Generating changes...",
+                    "Refining your script...",
+                  ]}
+                  duration={3000}
+                />
+              </div>
+            )}
           </div>
           <div className="flex items-start">
             <Textarea
@@ -107,16 +132,12 @@ const ReviewScript = ({
               onClick={askScriptAgent}
               className="shadow-lg cursor-pointer ml-2 p-3 bg-neutral-800 border border-transparent hover:border-pink-300 text-white transition-all duration-200 ease-in-out"
             >
-              {generatingChanges ? (
-                <LuLoaderCircle className="size-5 animate-spin" />
-              ) : (
-                <RiSendPlaneLine className="size-5" />
-              )}
+              <RiSendPlaneLine className="size-5" />
             </Button>
           </div>
         </div>
       </section>
-      <section className="w-full overflow-hidden p-6 flex flex-col">
+      <section className="relative w-full overflow-hidden p-6 flex flex-col">
         <h1 className="text-sm text-center font-bold text-white bg-neutral-900/60 px-4 py-3 rounded-md shadow-lg mb-2">
           Script
         </h1>
@@ -124,13 +145,25 @@ const ReviewScript = ({
           value={script}
           onChange={(e) => setScript(e.target.value)}
           placeholder="Edit your script here..."
-          className="flex-1 overflow-y-auto bg-neutral-900/60 backdrop-blur-sm rounded-md border border-neutral-600 shadow-lg text-white p-4"
+          className={`flex-1 overflow-y-auto bg-neutral-900/60 backdrop-blur-sm rounded-md border border-neutral-600 shadow-lg text-white p-4 ${generatingChanges && "blur-sm"}`}
           style={{ resize: "none" }}
         />
-        <Button className="py-6 cursor-pointer font-medium bg-neutral-800 border border-transparent hover:border-pink-300 text-pink-300 text-sm transition-all duration-200 ease-in-out mt-4">
+        <Button
+          disabled={generatingChanges}
+          className="py-6 cursor-pointer font-medium bg-neutral-800 border border-transparent hover:border-pink-300 text-pink-300 text-sm transition-all duration-200 ease-in-out mt-4"
+        >
           <CgMediaPodcast className="mr-2 size-5" />
           Generate Podcast
         </Button>
+
+        {generatingChanges && (
+          <div className="animate-pulse absolute inset-0 z-10 flex items-center justify-center bg-black/60 backdrop-blur-sm rounded-md transition-all">
+            <div className="text-white text-sm font-medium">
+              <LuLoaderCircle className="inline-block mr-2 size-4 animate-spin" />
+              Generating changes...
+            </div>
+          </div>
+        )}
       </section>
     </div>
   );
