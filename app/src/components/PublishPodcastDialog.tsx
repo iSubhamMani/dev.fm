01ced03e41374ff080a3615caf214ebfa6e4bb03
Subham Mani <manisubham09@gmail.com>
2025-06-27 22:00:20 +0530

    enhance PublishPodcastDialog with improved file upload handling and form validation

--- app/src/components/PublishPodcastDialog.tsx
+++ app/src/components/PublishPodcastDialog.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { Button } from "./ui/button";
@@ -12,7 +13,7 @@ import {
 } from "@/components/ui/dialog";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { publishPodcastSchema } from "@/schemas/publishPodcast";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import {
@@ -25,15 +26,21 @@ import {
 } from "./ui/form";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { Id } from "../../convex/_generated/dataModel";
-import { Label } from "./ui/label";
+import axios from "axios";
+import { toast } from "sonner";
+import { error, success } from "@/utils/sonnerStyles";
+import { useState } from "react";
 
 const PublishPodcastDialog = ({ podcastId }: { podcastId: string }) => {
   const data = useQuery(api.podcasts.queries.getPodcastById, {
     podcastId: podcastId as Id<"podcasts">,
   });
+
+  const publishPodcast = useMutation(api.podcasts.mutations.publishPodcast);
+
   const publishPodcastForm = useForm<z.infer<typeof publishPodcastSchema>>({
     resolver: zodResolver(publishPodcastSchema),
     defaultValues: {
@@ -44,15 +51,54 @@ const PublishPodcastDialog = ({ podcastId }: { podcastId: string }) => {
           episode: episode.episode,
           title: episode.title,
         })) || [],
-      coverImage: null,
+      coverImage: undefined as FileList | undefined,
     },
   });
 
+  const [isPublishing, setIsPublishing] = useState(false);
+
   const onSubmit = async (data: z.infer<typeof publishPodcastSchema>) => {
     try {
-      publishPodcastForm.reset();
-    } catch (error) {
-      console.error("Error publishing podcast:", error);
+      setIsPublishing(true);
+      const fd = new FormData();
+      fd.append("coverImage", data.coverImage[0]);
+
+      const res = await axios.post("/api/upload", fd);
+
+      if (res.data && res.data.url) {
+        const coverImageUrl = res.data.url;
+        const podcastData = {
+          title: data.title,
+          description: data.description,
+          coverImage: coverImageUrl,
+          episodeTitles: data.episodeTitles.map((episode) => ({
+            episode: episode.episode,
+            title: episode.title,
+          })),
+        };
+
+        const isPublished = await publishPodcast({
+          podcastId: podcastId as Id<"podcasts">,
+          ...podcastData,
+          status: "published",
+        });
+
+        if (isPublished) {
+          toast.success("Podcast published successfully!", {
+            style: success,
+            duration: 3000,
+            position: "top-center",
+          });
+        }
+      }
+    } catch {
+      toast.error("Error publishing podcast", {
+        style: error,
+        duration: 3000,
+        position: "top-center",
+      });
+    } finally {
+      setIsPublishing(false);
     }
   };
 
@@ -61,7 +107,7 @@ const PublishPodcastDialog = ({ podcastId }: { podcastId: string }) => {
       <DialogTrigger asChild>
         <Button className="mb-4 py-6 cursor-pointer font-medium bg-neutral-800 border border-transparent hover:border-pink-300 text-pink-300 text-sm transition-all duration-200 ease-in-out mt-4">
           <Radio className="mr-1 size-5" />
-          Publish
+          Continue to Publish
         </Button>
       </DialogTrigger>
       <DialogContent className="py-4 px-1 sm:p-6 bg-neutral-900/80 backdrop-blur-sm text-white border border-neutral-800 max-w-2xl w-full">
@@ -85,6 +131,7 @@ const PublishPodcastDialog = ({ podcastId }: { podcastId: string }) => {
                     <FormLabel className="text-neutral-300">Title</FormLabel>
                     <FormControl>
                       <Input
+                        required
                         className="bg-neutral-900/80 text-white placeholder:text-neutral-500"
                         {...field}
                         type="text"
@@ -105,6 +152,7 @@ const PublishPodcastDialog = ({ podcastId }: { podcastId: string }) => {
                     </FormLabel>
                     <FormControl>
                       <Textarea
+                        required
                         className="border bg-neutral-900/80 text-white placeholder:text-neutral-500 h-40"
                         {...field}
                         placeholder="A brief description of your podcast"
@@ -130,6 +178,7 @@ const PublishPodcastDialog = ({ podcastId }: { podcastId: string }) => {
                         </FormLabel>
                         <FormControl>
                           <Input
+                            required
                             {...field}
                             type="text"
                             placeholder={`Episode ${episode.episode} Title`}
@@ -146,62 +195,61 @@ const PublishPodcastDialog = ({ podcastId }: { podcastId: string }) => {
                   No episodes available to edit titles.
                 </p>
               )}
-              <Controller
+              <FormField
                 control={publishPodcastForm.control}
                 name="coverImage"
-                render={({ field: { onChange, value, ref } }) => (
-                  <div className="space-y-2">
-                    <div className="space-y-2">
-                      {/* Hidden input */}
-                      <Label
-                        className="text-neutral-300"
-                        htmlFor="customFileInput"
-                      >
-                        Cover Image
-                      </Label>
-                      <input
-                        type="file"
-                        accept="image/jpeg, image/png, image/jpg"
-                        onChange={(e) => onChange(e.target.files?.[0])}
-                        ref={ref}
-                        className="hidden"
-                        id="customFileInput"
-                      />
-
-                      {/* Custom Button */}
-                      <Button
-                        onClick={(e) => {
-                          e.preventDefault();
-                          const input = document.getElementById(
-                            "customFileInput"
-                          ) as HTMLInputElement;
-                          input.click();
-                        }}
-                        type="button"
-                        variant="secondary"
-                        className="cursor-pointer font-medium bg-neutral-800 border border-transparent hover:bg-neutral-900 hover:border-pink-300 text-neutral-300 text-xs transition-all duration-200 ease-in-out"
-                      >
-                        Choose File
-                      </Button>
-                    </div>
-
-                    {/* Show selected file name (optional) */}
-                    {value && (
-                      <p className="text-sm text-neutral-400 line-clamp-1">
-                        {value.name.length > 50
-                          ? `${value.name.slice(0, 50)}...`
-                          : value.name}
+                render={({ field }) => (
+                  <FormItem className="space-y-1">
+                    <FormLabel
+                      htmlFor="coverImageInput"
+                      className="text-neutral-300"
+                    >
+                      Cover Image
+                    </FormLabel>
+                    <FormControl>
+                      <div className="flex items-center gap-2">
+                        <Input
+                          type="file"
+                          accept="image/jpeg, image/png, image/jpg"
+                          onChange={(e) => field.onChange(e.target.files)}
+                          className="hidden"
+                          id="coverImageInput"
+                        />
+                        <Button
+                          onClick={(e) => {
+                            e.preventDefault();
+                            const input = document.getElementById(
+                              "coverImageInput"
+                            ) as HTMLInputElement;
+                            input.click();
+                          }}
+                          type="button"
+                          variant="secondary"
+                          className="cursor-pointer font-medium bg-neutral-800 border border-transparent hover:bg-neutral-900 hover:border-pink-300 text-neutral-300 text-xs transition-all duration-200 ease-in-out"
+                        >
+                          Choose File
+                        </Button>
+                      </div>
+                    </FormControl>
+                    {field.value && field.value.length > 0 && (
+                      <p className="text-sm text-neutral-400">
+                        {field.value[0].name.length > 40
+                          ? `${field.value[0].name.slice(0, 40)}...`
+                          : field.value[0].name}
                       </p>
                     )}
-                  </div>
+                    <FormMessage />
+                  </FormItem>
                 )}
               />
+
               <div className="flex justify-end">
                 <Button
+                  disabled={isPublishing}
                   type="submit"
                   className="cursor-pointer font-medium bg-neutral-800 border border-transparent hover:border-pink-300 text-pink-300 text-sm transition-all duration-200 ease-in-out"
                 >
-                  Publish
+                  {isPublishing ? "Hang Tight..." : "Publish"}
                 </Button>
               </div>
             </form>
